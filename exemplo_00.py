284d616f50384eccb8e6cf0403ca5f24fe2e8c5d
Rafael <sathler_754@hotmail.com>
2024-09-04 22:01:58 -0300

    Salvando todos os arquivos

---
+++ exemplo_00.py
@@ -0,0 +1,3 @@
+#Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+print(int(input("Digite o primeiro número:")) + int(input("Digite o segundo número:")))
---
+++ exemplo_03.py
@@ -0,0 +1,5 @@
+# Exercício 03: Refatore o exercício 02 atribuindo variáveis
+
+nome = input("Qual o seu nome? ")
+qnt_caract = len(nome)
+print(qnt_caract)
